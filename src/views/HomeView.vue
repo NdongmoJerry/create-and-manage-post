b7f137226ae808c4167995f72384a863fd1e3fc7
Ndongmo Jerry <ndongmojerry@mail.com>
2023-10-21 07:52:35 -0400

    initial files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       <div>
+        <CreatePost />
+       </div>
+    </div>
+  </template>
+  
+  <script>
+  import  CreatePost from '../components/CreatePost.vue';
+  export default {
+    name: 'HomeView',
+    components:{
+      CreatePost
+  
+    }
+  }
+  </script>
+  
