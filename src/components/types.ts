4ef927cda4a3eeafc4afa8867e815e09ab611523
Ndongmo Jerry <ndongmojerry@mail.com>
2023-11-13 15:30:34 -0500

    Fix issue with updating tags of a post

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,8 +1,8 @@
 export interface Post {
-    id?: number;
-    image: string;
-    tags: string[];
-    description: string;
-    created_at?: string;
-    updated_at?: string;
-  }
+  id?: number;
+  image: string;
+  tags: string[];
+  description: string;
+  created_at?: string;
+  updated_at?: string;
+}
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,17 +1,18 @@
 export interface Post {
-    id: string;
-    image: string;
-    selectedTags: string[];
-    description: string;
-    created_at: string;
-    updated_at: string; 
+  id: string;
+  image: string;
+  tags: string[];
+  selectedTags: string[];
+  description: string;
+  created_at: string;
+  updated_at: string; 
+}
+  
+  export interface AppState {
+    posts: Post[];
+    showDeleteAlert: boolean;
+    showCreateAlert: boolean;
+    selectedPost: Post;
+    allTags: string[];
   }
-    
-    export interface AppState {
-      posts: Post[];
-      showDeleteAlert: boolean;
-      showCreateAlert: boolean;
-      selectedPost: Post;
-      allTags: string[];
-    }
-   
+ 
